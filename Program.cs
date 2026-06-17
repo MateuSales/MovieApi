7b6148fad23a587270c469ed4a8366019762a655
Mateus Sales <matheussalles40@gmail.com>
2026-06-17 20:27:18 -0300

    feat(mateus): adicionando o projeto gerado

---
+++ Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using MovieApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configuração do MySQL
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
